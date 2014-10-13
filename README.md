3d219cbafdc4768d5016abf59e9056e0c2960b9d
Strabox <pardal.pires@gmail.com>
2014-10-13 17:18:24 +0100

    hmmm

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 IAh
+
+Projecto para a Cadeira de Iteligência Artificial 2014/2015 1º Semestre.
 ===
---
+++ main.lisp
@@ -0,0 +1,16 @@
+;Projecto IA
+;Grupo: 
+;Andre Filipe Pardal Pires			Nº76046
+;Miguel Oliveira Melicia Martins 	Nº76102
+;Artur....							Nº75...
+
+;================== Estruturas de Dados =====================
+
+; TIPO RESTRICAO
+ 
+
+
+
+; TIPO PSR
+
+;================ Fim Estruturas de Dados ===================
